3c72da915e5b1cfeaebb2e34366ba202338f7034
Lucas Cordeiro <ecdb.lucas@gmail.com>
2021-04-14 18:02:10 -0300

    Add DKIM support and enable logging

--- src/modules/mail/transporter.ts
+++ src/modules/mail/transporter.ts
@@ -4,34 +4,26 @@ import type Mail from 'nodemailer/lib/mailer'
 import config from '../../config'
 
 const transport = nodemailer.createTransport({
+  // @ts-ignore
   host: config.MAIL_HOST,
-  secure: true,
-  port: 465,
+  secure: false,
+  requireTLS: true,
+  logger: true,
+  port: 587,
   auth: {
     user: config.MAIL_USERNAME,
     pass: config.MAIL_PASSWORD,
   },
-})
-
-const mailServerReadyPromise = new Promise<void>((resolve) => {
-  transport.verify((err) => {
-    if (err !== null) {
-      console.log('Error connecting to email server: ' + err)
-      return resolve()
-    }
-
-    console.log('Successfully connected to email server')
-    resolve()
-  })
+  dkim: config.DKIM_PRIVATE_KEY
+    ? {
+        domainName: config.DKIM_DOMAIN_NAME,
+        keySelector: config.DKIM_KEY_SELECTOR,
+        privateKey: config.DKIM_PRIVATE_KEY,
+      }
+    : undefined,
 })
 
 export const sendMail = async (options: Mail.Options) => {
-  try {
-    await mailServerReadyPromise
-  } catch {
-    return
-  }
-
   return new Promise<any>((resolve, reject) => {
     transport.sendMail(options, (err, info) => {
       if (err !== null) {
