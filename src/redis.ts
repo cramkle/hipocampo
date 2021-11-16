a7b3edb6157a35543df295bbdf41f9f59ef2a8c0
Edisson Reinozo <edisson350@gmail.com>
2021-11-16 11:46:52 -0500

    Clean redis

--- src/redis.ts
+++ src/redis.ts
@@ -1,4 +1,4 @@
-import { promises as dns } from 'dns'
+// import { promises as dns } from 'dns'
 
 import type { Redis } from 'ioredis'
 import redis from 'ioredis'
@@ -7,44 +7,60 @@ import config from './config'
 
 let redisClient: Redis | null = null
 
-let dnsResolvePromise: Promise<string[]> | null = null
+// let dnsResolvePromise: Promise<string[]> | null = null
 
 export async function getRedisInstance(): Promise<Redis> {
   if (redisClient !== null) {
     return redisClient
   }
 
-  const redisHosts = await (dnsResolvePromise ??
-    (dnsResolvePromise = dns.resolve(config.REDIS_HOST, 'A')))
+  // const redisHosts = await (dnsResolvePromise ??
+  //   (dnsResolvePromise = dns.resolve(config.REDIS_HOST, 'A')))
+  // console.log(
+  //   `dns.resolve(config.REDIS_HOST, 'A'): ${await(dns.resolve(
+  //     config.REDIS_HOST,
+  //     'A'
+  //   ))}`
+  // )
+  // console.log(`redisHosts.length: ${redisHosts.length}`)
+  // if (redisClient !== null) {
+  //   return redisClient
+  // }
+  // if (redisHosts.length > 1) {
+  //   const sentinelInstance = new redis({
+  //     sentinels: redisHosts.map((host) => ({
+  //       host,
+  //       port: config.REDIS_SENTINEL_PORT,
+  //     })),
+  //     sentinelPassword: config.REDIS_PASSWORD,
+  //     host: config.REDIS_HOST,
+  //     db: config.REDIS_DB,
+  //     password: config.REDIS_PASSWORD,
+  //     name: config.REDIS_SENTINEL_NAME,
+  //   })
 
-  if (redisClient !== null) {
-    return redisClient
-  }
+  //   redisClient = sentinelInstance
+  // } else {
+  //   const redisInstance = new redis({
+  //     host: config.REDIS_HOST,
+  //     port: config.REDIS_PORT,
+  //     db: config.REDIS_DB,
+  //     password: config.REDIS_PASSWORD,
+  //   })
 
-  if (redisHosts.length > 1) {
-    const sentinelInstance = new redis({
-      sentinels: redisHosts.map((host) => ({
-        host,
-        port: config.REDIS_SENTINEL_PORT,
-      })),
-      sentinelPassword: config.REDIS_PASSWORD,
-      host: config.REDIS_HOST,
-      db: config.REDIS_DB,
-      password: config.REDIS_PASSWORD,
-      name: config.REDIS_SENTINEL_NAME,
-    })
+  //   redisClient = redisInstance
+  // }
 
-    redisClient = sentinelInstance
-  } else {
-    const redisInstance = new redis({
-      host: config.REDIS_HOST,
-      port: config.REDIS_PORT,
-      db: config.REDIS_DB,
-      password: config.REDIS_PASSWORD,
-    })
+  // redisClient.on('error', (error) => {
+  //   console.error('[REDIS ERROR]', error)
+  // })
 
-    redisClient = redisInstance
-  }
+  redisClient = new redis({
+    host: config.REDIS_HOST,
+    port: config.REDIS_PORT,
+    db: config.REDIS_DB,
+    password: config.REDIS_PASSWORD,
+  })
 
   redisClient.on('error', (error) => {
     console.error('[REDIS ERROR]', error)
