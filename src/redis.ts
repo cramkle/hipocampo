5d2994038fc5ef40e3b44fec45c50855d33542ad
Lucas Cordeiro <ecdb.lucas@gmail.com>
2023-03-14 20:10:51 -0300

    Simplify Redis connection and update dependencies

--- src/redis.ts
+++ src/redis.ts
@@ -1,50 +1,26 @@
-import { promises as dns } from 'dns'
-
 import type { Redis } from 'ioredis'
-import redis from 'ioredis'
+import RedisImpl from 'ioredis'
 
 import config from './config'
 
 let redisClient: Redis | null = null
 
-let dnsResolvePromise: Promise<string[]> | null = null
-
-export async function getRedisInstance(): Promise<Redis> {
+export function getRedisInstance(): Redis {
   if (redisClient !== null) {
     return redisClient
   }
 
-  const redisHosts = await (dnsResolvePromise ??
-    (dnsResolvePromise = dns.resolve(config.REDIS_HOST, 'A')))
-
-  if (redisClient !== null) {
-    return redisClient
+  const redisOptions = {
+    host: config.REDIS_HOST,
+    db: config.REDIS_DB,
+    password: config.REDIS_PASSWORD,
   }
 
-  if (redisHosts.length > 1) {
-    const sentinelInstance = new redis({
-      sentinels: redisHosts.map((host) => ({
-        host,
-        port: config.REDIS_SENTINEL_PORT,
-      })),
-      sentinelPassword: config.REDIS_PASSWORD,
-      host: config.REDIS_HOST,
-      db: config.REDIS_DB,
-      password: config.REDIS_PASSWORD,
-      name: config.REDIS_SENTINEL_NAME,
-    })
+  const redisInstance = new RedisImpl({
+    ...redisOptions,
+  })
 
-    redisClient = sentinelInstance
-  } else {
-    const redisInstance = new redis({
-      host: config.REDIS_HOST,
-      port: config.REDIS_PORT,
-      db: config.REDIS_DB,
-      password: config.REDIS_PASSWORD,
-    })
-
-    redisClient = redisInstance
-  }
+  redisClient = redisInstance
 
   redisClient.on('error', (error) => {
     console.error('[REDIS ERROR]', error)
